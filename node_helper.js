fb6cb01b5f3f418846dc18df726382b0519a36a1
Sakir Temel <sakirtemel@gmail.com>
2016-11-08 19:25:19 +0100

    Replace start button library

--- node_helper.js
+++ node_helper.js
@@ -1,29 +1,36 @@
 var NodeHelper = require("node_helper");
 
-var pin = 18;
+var pin = 4;
 
 module.exports = NodeHelper.create({
     start: function() {
         this.config = null;
+        this.lastPressedTime = null;
     },
     socketNotificationReceived: function(notification, payload) {
         if(notification === 'SET_CONFIG'){
             this.config = payload;
 
             if(this.config['enableRaspberryButton']){
-                var gpio = require("pi-gpio");
+                var Gpio = require('onoff').Gpio,
+                button = new Gpio(pin, 'in', 'both');
 
-                while(true){
-                    gpio.open(pin, "input", function(err) {
-                        gpio.read(pin, function(err, value) {
-                            console.log(value);
-                            if(value === 1){
-                                this.sendSocketNotification('ALEXA_START_RECORDING', {});
-                                // TODO: sleep for few ms not to send multiple start commands
-                            }
-                        });
-                    });
-                }
+                process.on('SIGINT', function () {
+                    button.unexport();
+                });
+
+                var self = this;
+
+                button.watch(function (err, value) {
+                    if (err) { throw err; }
+
+                    if(value === 1){
+                        if(self.lastPressedTime === null || (self.lastPressedTime !== null && (new Date() - self.lastPressedTime) > 2)){
+                            self.lastPressedTime = new Date();
+                            self.sendSocketNotification('ALEXA_START_RECORDING', {});
+                        }
+                    }
+                });
             }
         }
     },
